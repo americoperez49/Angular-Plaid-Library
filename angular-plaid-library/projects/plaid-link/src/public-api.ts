f71c8b35f37b7c1b22c38dbff65b6c4734ca21b9
Americo Perez <burneremail104@gmail.com>
2025-09-20 11:54:36 -0500

    updated exports

--- angular-plaid-library/projects/plaid-link/src/public-api.ts
+++ angular-plaid-library/projects/plaid-link/src/public-api.ts
@@ -5,4 +5,5 @@
 export * from './lib/plaid-link.service';
 export * from './lib/plaid-link-script-loader.service';
 export * from './lib/plaid-link.types';
+export * from './lib/plaid-link-demo.component'; // Export the demo component
 export * from './lib/plaid-link'; // Assuming this might export something else, keep for now.
