867bfbb85930b317025057acc2a0e2f86096c080
Americo Perez <burneremail104@gmail.com>
2025-09-20 12:55:20 -0500

    updated the frontend app to use the new inputs and outputs

--- angular-plaid-appV2/src/app/components/test/test.ts
+++ angular-plaid-appV2/src/app/components/test/test.ts
@@ -8,10 +8,21 @@ import { PlaidLinkDemoComponent } from '../../../../../angular-plaid-library/pro
   styleUrl: './test.scss',
 })
 export class Test implements OnInit {
+  linkToken: string | null = null;
   constructor() {}
 
   ngOnInit(): void {
     //get the link token from the backend
     //pass it to the plaid-link component
+  }
+
+  onSuccess(event: any) {
+    console.log('Success event received in parent component:', event);
+  }
+  onExit(event: any) {
+    console.log('Exit event received in parent component:', event);
+  }
+  onEvent(event: any) {
+    console.log('Event received in parent component:', event);
   }
 }
